cc0974f78ed2ec236b79cfa9f546f90947172b46
Aravind Bharathi <62253557+aravindbharathi@users.noreply.github.com>
2021-02-16 15:11:34 +0530

    Update itriedsomething.js

--- js/itriedsomething.js
+++ js/itriedsomething.js
@@ -5,4 +5,34 @@ function myFunction() {
   } else {
     x.style.display = "none";
   }
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("myDIV3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("myDIV4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("myDIV5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("myDIV6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
